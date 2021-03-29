bc022850915b2b5a968b30bf92844b442e922e0a
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2021-03-29 13:45:27 -0400

    Create IT213 Unit 2 Assignment

---
+++ IT213 Software Development Concepts/IT213_King_Unit2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace IT213_KingJeremy_Unit2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //************************************************************************************
+            //***** Assignment 2 Section 1
+            //************************************************************************************
+
+            Console.WriteLine("Unit 2 Project\n\n");
+
+            // Initialize Variables
+
+            int wholeNumber1;
+            int wholeNumber2;
+
+            // Processing
+
+            // Display Results
+
+            Console.Write("Please Enter a Whole Nummber: \n");
+            Console.WriteLine();
+            wholeNumber1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Please Enter a Second Whole Nummber: \n");
+            Console.WriteLine();
+            wholeNumber2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            if (wholeNumber1 > wholeNumber2)
+                Console.WriteLine(wholeNumber1 + " > " + wholeNumber2);
+            if (wholeNumber1 < wholeNumber2)
+                Console.WriteLine(wholeNumber1 + " < " + wholeNumber2);
+            if (wholeNumber1 >= wholeNumber2)
+                Console.WriteLine(wholeNumber1 + " >= " + wholeNumber2);
+            if (wholeNumber1 <= wholeNumber2)
+                Console.WriteLine(wholeNumber1 + " <= " + wholeNumber2);
+            if (wholeNumber1 == wholeNumber2)
+                Console.WriteLine(wholeNumber1 + " == " + wholeNumber2);
+            if (wholeNumber1 != wholeNumber2)
+                Console.WriteLine(wholeNumber1 + " != " + wholeNumber2);
+
+
+            //************************************************************************************
+            //***** Assignment 2 Section 2
+            //************************************************************************************
+
+            // Initialize Variables
+
+            int userGrade;
+            
+            Console.Write("Please Enter a Grade {A - F}: \n");
+            userGrade = Convert.ToInt32(Console.ReadLine());
+
+            if (userGrade >= 60)
+                Console.WriteLine("Congratulations, you passed.");
+            else
+                Console.WriteLine("Sorry, you failed.");
+
+            // Processing
+
+            // Display Results
+
+
+            //************************************************************************************
+            //***** Assignment 2 Section 1
+            //************************************************************************************
+
+            // Initialize Variables
+
+
+
+            // End of Program
+            Console.Read();
+        }
+    }
+}
