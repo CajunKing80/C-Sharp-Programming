fb6cc6994c11a3e8f263b4e5e77dd94cc6a2c9ba
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2021-04-07 17:46:05 -0400

    Create IT213 unit 3 assignment. Section 1

--- IT213 Software Development Concepts/IT213_King_unit3/Program.cs
+++ IT213 Software Development Concepts/IT213_King_unit3/Program.cs
@@ -6,7 +6,33 @@ namespace IT213_King_unit3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //************************************************************************************
+            //***** Assignment 3 Section 1
+            //************************************************************************************
+
+            Console.WriteLine("**** Assignment 3: Section 1 - While Loop to Average Grades ****\n");
+
+            // Initialize the Variables
+
+            int counter = 1;
+            int total = 0;
+            int grade = 0;
+            int average = 0;
+
+
+            while (counter <= 10)
+            {
+                Console.Write("Please Enter a Grade: ");
+                grade = Convert.ToInt32(Console.ReadLine());
+                total = total + grade;
+                counter += 1;
+            }
+
+            average = total / 10;
+
+            Console.WriteLine("The Total For All 10 Grades Is: " + total);
+            Console.WriteLine("The Class Average Is: " + average);
         }
     }
 }
