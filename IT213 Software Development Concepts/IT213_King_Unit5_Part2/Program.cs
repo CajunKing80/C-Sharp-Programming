02343ef2c15ed42bc77dc75b1bd5dbab31f7024e
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2021-04-23 17:54:16 -0400

    4-23 commits v1

---
+++ IT213 Software Development Concepts/IT213_King_Unit5_Part2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT213_King_Unit5_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "The quick brown fox jumps over the lazy dog";
+            int current = 0;
+            int blankPosition = 0;
+            int wordcount = 0;
+            string word;
+
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                current = i;
+                blankPosition = sentence.IndexOf(" ", current);
+
+                if (blankPosition == -1)
+                {
+                    word = sentence.Substring(current);
+                    i = sentence.Length;
+                }
+                else
+                {
+                    word = sentence.Substring(current, blankPosition - current);
+                    i = blankPosition; 
+                }
+
+                wordcount++;
+                Console.WriteLine(wordcount + "  " + word);
+            }
+            Console.WriteLine("Total word count " + wordcount);
+            Console.ReadKey();
+        }
+    }
+}
