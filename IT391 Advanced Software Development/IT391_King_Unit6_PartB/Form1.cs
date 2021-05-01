2308a8aa5c9ff2ce9953ccc24ce8cf9192330fb0
CajunKing80 <78227227+CajunKing80@users.noreply.github.com>
2021-05-01 10:32:52 -0400

    IT391 Unit 6 Assignment commits

---
+++ IT391 Advanced Software Development/IT391_King_Unit6_PartB/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IT391_King_Unit6_PartB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += new EventHandler(button1_Click);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length < 15)
+
+                if (textBox2.Text.Length < 15)
+
+                    if (textBox3.Text != null)
+
+                        if (textBox4.Text != null)
+
+                            if (textBox5.Text.Length == 5)
+                            {
+                                string firstName = textBox1.Text;
+                                string lastName = textBox2.Text;
+                                string address = textBox4.Text;
+                                long phoneNumber = long.Parse(textBox3.Text);
+                                int zipCode = int.Parse(textBox5.Text);
+                                MessageBox.Show("Submitted Succesfully");
+                            }
+
+                            else
+                            {
+                                MessageBox.Show("Input Validation Error");    
+                            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
